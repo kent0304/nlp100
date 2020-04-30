9142eb6539dd850a61978677c93f8d43dd04b0ce
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-04-30 12:14:13 +0900

    done 12

---
+++ ch02/ex12.py
@@ -0,0 +1,34 @@
+### ex12.py
+### 1列目をcol1.txtに，2列目をcol2.txtに保存
+
+##--------pyhton------------
+
+# それぞれの行の専用リストを用意
+row1 = []
+row2 = []
+with open ('popular-names.txt') as f:
+    # 各行を変数lineに代入
+    for line in f:
+        # splitで区切ったリストをstring_listに代入
+        string_list=line.split()
+        # string_listの1列目・2列目をappend
+        row1.append(string_list[0])
+        row2.append(string_list[1])
+
+# 別ファイル名で保存
+with open ('col1.txt', mode="w") as f:
+    for line in row1:
+        # 各行の値と改行を同時に書き込み
+        f.write(line+"\n")
+
+# 別ファイル名で保存
+with open ('col2.txt', mode="w") as f:
+    for line in row2:
+        f.write(line+"\n")
+
+##---------end--------------
+
+##---------UNIX-------------
+# -fオプションは区切られた項目の中で，表示したい項目を指定
+# cut -f (項目数) (ファイル名)
+##---------end--------------
