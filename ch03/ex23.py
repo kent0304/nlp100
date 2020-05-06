c7624d87aacccdf6476752f7045cf9241cdc9fc1
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-05-06 16:22:15 +0900

    do ex23

---
+++ ch03/ex23.py
@@ -0,0 +1,26 @@
+import jsonload
+british_text = jsonload.british_text
+
+import re
+
+# 文字列リテラルの頭にrをつけると，エスケープシーケンスが無効
+# findallは重複しない全てのマッチを文字列リストで表す
+# british_textは複数行からなるので，re.MULTILINEを指定すると文字列の先頭と末尾にもアクセスできる
+result = re.findall(r'^(.*\==+.*==+.*)$', british_text, flags=re.MULTILINE)
+
+
+# 各行において'==' が入っていればカウントしレベルを付与
+for line in result:
+    if '=====' in line:
+        line = line.replace('=====', '')
+        line += ' ' + '4'
+    elif '====' in line:
+        line = line.replace('====', '')
+        line += ' ' + '3'
+    elif '===' in line:
+        line = line.replace('===', '')
+        line += ' ' + '2'
+    elif '==' in line:
+        line = line.replace('==', '')
+        line += ' ' + '1'
+    print(line)
--- ch03/jsonload.py
+++ ch03/jsonload.py
@@ -11,3 +11,8 @@ with open('jawiki-country.json', 'r') as f:
         # raw_decodeはJSON文書をデコードし，pythonでの表現と文末のインデックスの2要素を返す
         res.append(decoder.raw_decode(line))
         line = f.readline()
+
+for content in res:
+    # 各contentは2つの要素が入ったtuple型
+    if (content[0]['title']=='イギリス'):
+        british_text = content[0]['text']
