1899473ebe58c24a940cb721baba16d29de9f6af
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-05-07 00:23:21 +0900

    do ex25

--- ch03/ex25.py
+++ ch03/ex25.py
@@ -6,8 +6,13 @@ import re
 # 文字列リテラルの頭にrをつけると，エスケープシーケンスが無効
 # findallは重複しない全てのマッチを文字列リストで表す
 # ()で囲むことでキャプチャし，()で囲まれたものをそれぞれグループにして抽出できる
-# (?:  )は()のキャプチャしないバージョン
-result = re.findall(r'(?:ファイル):(.+?)\|', british_text)
+# re.DOTALLを足すと.に改行も対象となる
+result = re.findall(r'^\{\{基礎情報.*?$(.*?)^\}\}$', british_text, flags=re.MULTILINE + re.DOTALL)
+new_result = re.findall(r'^\|(.+?)\s*=\s*(.+?)(?:(?=\n\|)|(?=\n$))', result[0], flags=re.MULTILINE + re.DOTALL)
 
-for line in result:
-    print(line)
+dict = {}
+
+for line in new_result:
+    dict[line[0]] = line[1]
+
+print(dict)
