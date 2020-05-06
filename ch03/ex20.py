7dd6952c1136375db9e8a409d5bb2a78182fb66a
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-05-06 12:58:17 +0900

    did ex20.py

---
+++ ch03/ex20.py
@@ -0,0 +1,19 @@
+import json
+
+decoder = json.JSONDecoder()
+res=[]
+
+# rはreadのr．json読み込んで開く
+with open('jawiki-country.json', 'r') as f:
+    # readlineで1行毎に読み込む
+    line = f.readline()
+    while line:
+        # raw_decodeはJSON文書をデコードし，pythonでの表現と文末のインデックスの2要素を返す
+        res.append(decoder.raw_decode(line))
+        line = f.readline()
+
+
+for content in res:
+    # 各contentは2つの要素が入ったtuple型
+    if (content[0]['title']=='イギリス'):
+        print(content[0]['text'])
