32405f55aa23d55b4e7bf87c10022151aaa8d3ae
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-05-14 22:06:04 +0900

    do ex33

---
+++ ch04/ex33.py
@@ -0,0 +1,48 @@
+import MeCab
+
+# MeCabの出力フォーマット
+# 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用型,活用形,原形,読み,発音
+
+with open('neko.txt.mecab') as f:
+    ma_list = f.readlines()
+
+total_list =[]
+for ma in ma_list:
+    # 形態素一つごとにdictionary用意
+    ma_dict = {}
+    # \tで分割した最初の要素
+    ma_dict['surface'] = ma.split('\t')[0]
+    # \tで分割した後ろの要素をさらに','で分割
+
+    # ma.split('\t')でうまく分けられないものもあるので条件分岐
+    if len(ma.split('\t'))>=2:
+        info = ma.split('\t')[1]
+        ma_dict['base'] = info.split(',')[6]
+        ma_dict['pos'] = info.split(',')[0]
+        ma_dict['pos1'] = info.split(',')[1]
+        # 形態素の情報をtotal_listに追加
+        total_list.append(ma_dict)
+
+# -------- ここからex32 ------------------
+norn_surface=[]
+for ma_dict in total_list:
+    if ma_dict['pos'] == '名詞':
+        norn_surface.append(ma_dict['surface'])
+
+print(norn_surface)
+
+# 「AのB」を格納するリスト
+linked_norn=[]
+for idx in range(len(total_list)-2):
+    # 先頭の形態素
+    prev_dict = total_list[idx]
+    # 次の形態素
+    middle_dict = total_list[idx+1]
+    # 次の次の形態素
+    next_dict = total_list[idx+2]
+    if prev_dict['pos'] == '名詞':
+        if middle_dict['surface'] == 'の':
+            if next_dict['pos'] == '名詞':
+                linked_norn.append(prev_dict['surface'] + middle_dict['surface'] + next_dict['surface'])
+
+print(linked_norn)
