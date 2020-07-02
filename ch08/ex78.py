bf1b332e8ff8327f8da28a9bb05e463c9dc655c7
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-07-02 16:49:21 +0900

    finish ex79

---
+++ ch08/ex78.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from matplotlib import pyplot as plt
+import time
+
+# 学習データ用意
+x_train = pd.read_csv('data/X_train.txt',sep=' ', header=None)
+x_valid = pd.read_csv('data/X_valid.txt',sep=' ', header=None)
+x_test = pd.read_csv('data/X_test.txt',sep=' ', header=None)
+# convert pandas df to tensor
+x_train = torch.tensor(x_train.values, dtype=torch.float)
+x_valid = torch.tensor(x_valid.values, dtype=torch.float)
+x_test = torch.tensor(x_test.values, dtype=torch.float)
+
+# 教師ラベル用意
+y_train = pd.read_csv('data/y_train.txt', header=None)
+y_valid = pd.read_csv('data/y_valid.txt', header=None)
+y_test = pd.read_csv('data/y_test.txt', header=None)
+# convert pandas df to tensor
+y_train = torch.tensor(y_train.values, dtype=torch.long).flatten()
+y_valid = torch.tensor(y_valid.values, dtype=torch.long).flatten()
+y_test = torch.tensor(y_test.values, dtype=torch.long).flatten()
+
+
+class NN(nn.Module):
+    def __init__(self, input=300, output=4):
+        super(NN, self).__init__()
+        self.fc1 = nn.Linear(input, output, bias=False)
+        nn.init.normal_(self.fc1.weight, mean=0.0, std=1.0) # 正規分布で重みを初期化
+
+    def forward(self, x):
+        return self.fc1(x)
+
+# データセットの作成
+class MyDataset(Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __len__(self):
+        return len(self.y)
+
+    # インデックスidxで返り値指定
+    def __getitem__(self, idx):
+        return [self.x[idx], self.y[idx]]
+
+# define accuracy
+def calculate_loss_and_accuracy(model, loss_function, loader):
+    model.eval()
+    loss = 0.0
+    correct = 0
+    cnt = 0
+    with torch.no_grad():
+        for inputs, labels in loader:
+            outputs = model.forward(inputs)
+            loss += loss_function(outputs, labels).item()
+            pred = torch.argmax(outputs, dim=-1)
+            correct += (pred == labels).sum().item()
+            cnt += len(inputs)
+
+    return loss / len(loader), correct / cnt
+
+def train_model(dataset_train, dataset_valid, batch_size, model, loss_function, optimizer, num_epochs, device=None):
+    # GPUにおくる
+    model.to(device)
+
+    # Prepare DataLoader
+    dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+    dataloader_valid = DataLoader(dataset_valid, batch_size=len(dataset_valid), shuffle=False)
+    # dataloader_test = DataLoader(dataset_test, batch_size=len(dataset_test), shuffle=False)
+
+    # Training
+    log_train = []
+    log_valid = []
+    for epoch in range(num_epochs):
+        # 開始時刻の記録
+        s_time = time.time()
+        # start training
+        model.train()
+        loss_train = 0.0
+        for i, (inputs, labels) in enumerate(dataloader_train):
+            # 勾配をゼロで初期化
+            optimizer.zero_grad()
+
+            # 順伝播 + 誤差逆伝播 + 重み更新
+            outputs = model.forward(inputs)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+        # calculate accuracy
+        loss_train, acc_train = calculate_loss_and_accuracy(model, loss_function, dataloader_train)
+        loss_valid, acc_valid = calculate_loss_and_accuracy(model, loss_function, dataloader_valid)
+        log_train.append([loss_train, acc_train])
+        log_valid.append([loss_valid, acc_valid])
+
+        # save checkpoints
+        torch.save({'epoch': epoch, 'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict()}, f'checkpoints/checkpoint{epoch+1}.pt')
+        # 終了時刻の記録
+        e_time = time.time()
+        # output logs
+        print(f'epoch: {epoch + 1}, loss_train:{loss_train:.4f}, acc_train:{acc_train:.3f}, loss_valid:{loss_valid:.4f}, acc_valid:{acc_valid:.3f}, {(e_time - s_time):.4f}sec')
+    return {'train': log_train, 'valid': log_valid}
+
+
+# define a model
+model = NN()
+# define a loss function
+loss_function = nn.CrossEntropyLoss()
+# define a optimizer
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+
+# Prepare Dataset
+dataset_train = MyDataset(x_train, y_train)
+dataset_valid = MyDataset(x_valid, y_valid)
+dataset_test = MyDataset(x_test, y_test)
+
+# デバイスの指定
+device = torch.device('cuda')
+
+# training a model
+for batch_size in [2**i for i in range(11)]:
+    print(f'バッチサイズ: {batch_size}')
+    log =  train_model(dataset_train, dataset_valid, batch_size, model, loss_function, optimizer, 1, device = device)
+
+# # visualization
+# fig, ax = plt.subplots(1,2, figsize=(15,5))
+# ax[0].plot(np.array(log_train).T[0], label='train')
+# ax[0].plot(np.array(log_valid).T[0], label='valid')
+# ax[0].set_xlabel('epoch')
+# ax[0].set_ylabel('loss')
+# ax[0].legend()
+# ax[1].plot(np.array(log_train).T[1], label='train')
+# ax[1].plot(np.array(log_valid).T[1], label='valid')
+# ax[1].set_xlabel('epoch')
+# ax[1].set_ylabel('accuracy')
+# ax[1].legend()
+# plt.show()
