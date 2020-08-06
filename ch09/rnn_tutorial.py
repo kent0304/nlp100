662c1aaff3f3a74c417e2d7e18373e60370ca4cb
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-08-06 20:47:07 +0900

    finish editing

--- ch09/rnn_tutorial.py
+++ ch09/rnn_tutorial.py
@@ -71,3 +71,186 @@ def lineToTensor(line):
 # print(lineToTensor("Kent"))
 
 # --------------------------------------------------------------------------------
+# create the network
+import torch.nn as nn
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+n_hidden = 128 # 任意の数に設定
+rnn = RNN(n_letters, n_hidden, n_categories) # 入力は文字数 出力はカテゴリ数
+
+# --------------------------------------------------------------------------------
+# Training Preparation
+input = letterToTensor('A')
+hidden =torch.zeros(1, n_hidden)
+
+output, next_hidden = rnn(input, hidden)
+
+def categoryFromOutput(output):
+    top_n, top_i = output.topk(1)
+    category_i = top_i[0].item()
+    return all_categories[category_i], category_i
+
+# print(categoryFromOutput(output))
+
+import random
+
+def randomChoice(l):
+    return l[random.randint(0, len(l)-1)]
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    line_tensor = lineToTensor(line)
+    return category, line, category_tensor, line_tensor
+
+# for i in range(10):
+#     category, line, category_tensor, line_tensor = randomTrainingExample()
+#     print('category =', category, '/ line =', line)
+
+# --------------------------------------------------------------------------------
+# Training the network
+criterion = nn.NLLLoss()
+learning_rate = 0.01
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+
+    rnn.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    for p in rnn.parameters():
+        p.data.add_(p.grad.data, alpha=-learning_rate)
+
+    return output, loss.item()
+
+
+import time
+import math
+
+n_iters = 100000
+print_every = 5000
+plot_every = 1000
+
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m,s)
+
+start = time.time()
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output, loss =  train(category_tensor, line_tensor)
+    current_loss += loss
+
+    if iter % print_every ==0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
+
+# --------------------------------------------------------------------------------
+# Plotting the Results
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+#
+# # --------------------------------------------------------------------------------
+# # # Evaluating the Results
+# # Keep track of correct guesses in a confusion matrix
+# confusion = torch.zeros(n_categories, n_categories)
+# n_confusion = 10000
+#
+# Just return an output given a line
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output
+#
+# # Go through a bunch of examples and record which are correctly guessed
+# for i in range(n_confusion):
+#     category, line, category_tensor, line_tensor = randomTrainingExample()
+#     output = evaluate(line_tensor)
+#     guess, guess_i = categoryFromOutput(output)
+#     category_i = all_categories.index(category)
+#     confusion[category_i][guess_i] += 1
+#
+# # Normalize by dividing every row by its sum
+# for i in range(n_categories):
+#     confusion[i] = confusion[i] / confusion[i].sum()
+#
+# # Set up plot
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# cax = ax.matshow(confusion.numpy())
+# fig.colorbar(cax)
+#
+# # Set up axes
+# ax.set_xticklabels([''] + all_categories, rotation=90)
+# ax.set_yticklabels([''] + all_categories)
+#
+# # Force label at every tick
+# ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+# ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+#
+# # sphinx_gallery_thumbnail_number = 2
+# plt.show()
+
+
+# --------------------------------------------------------------------------------
+# prediction
+
+def predict(input_line, n_predictions=3):
+    print('\n> %s' % input_line)
+    with torch.no_grad():
+        output = evaluate(lineToTensor(input_line))
+
+        topv, topi = output.topk(n_predictions, 1, True)
+        predictions = []
+
+        for i in range(n_predictions):
+            value = topv[0][i].item()
+            category_index = topi[0][i].item()
+            print('(%.2f) %s' % (value, all_categories[category_index]))
+            predictions.append([value, all_categories[category_index]])
+
+
+predict('Tanaka')
